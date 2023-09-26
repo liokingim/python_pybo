4bb701e30993cade5f7936f9f3b435f6d293d8dd
liokingim <liokingim@daum.net>
2023-09-26 17:48:36 +0900

    로그인과 로그아웃

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from pybo import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("pybo/", include("pybo.urls")),
+    path("common/", include("common.urls")),
+    # '/' 에 해당되는 path
+    path("", views.index, name='index'),
 ]
