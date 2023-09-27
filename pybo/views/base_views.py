0c5190dcc3f2f00d93cbb638da828e2161e2f785
liokingim <liokingim@daum.net>
2023-09-28 01:02:33 +0900

    검색

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -1,17 +1,33 @@
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render
+from django.db.models import Q
 
 from ..models import Question
 
 def index(request):
     # 페이지
     page = request.GET.get('page', '1')
+    # 검색어
+    kw = request.GET.get('kw', '')
+    # 키워드의 좌우 공백 제거
+    kw = kw.strip()
+
     question_list = Question.objects.order_by('-create_date')
+
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |  # 제목 검색
+            Q(content__icontains=kw) |  # 내용 검색
+            Q(answer__content__icontains=kw) |  # 답변 내용 검색
+            Q(author__username__icontains=kw) |  # 질문 글쓴이 검색
+            Q(answer__author__username__icontains=kw)  # 답변 글쓴이 검색
+        ).distinct()
+
     # 페이지당 10개씩 보여 주기
     paginator = Paginator(question_list, 10)
     page_obj = paginator.get_page(page)
     # question_list는 페이징 객체(page_obj)
-    context = {'question_list': page_obj}
+    context = {'question_list': page_obj, 'page':page, 'kw':kw}
     return render(request, 'pybo/question_list.html', context)
 
 def detail(request, question_id):
